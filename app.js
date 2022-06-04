470362028e8a3abb1648dc9079e1a7460585bdd0
Nimesh <nimeshjohari95@gmail.com>
2022-06-04 21:07:54 +0530

    User Added TO DB  Update :)

--- app.js
+++ app.js
@@ -7,21 +7,33 @@ require("./models/DataEntry");
 dotenv.config({ path: "./.env" });
 require("./db/connection");
 const User = require("./models/User");
-
+const uuid = require("uuid");
 // 2. MIDDLEWARE
 const middleware = (req, res, next) => {
   console.log("middleware running");
   next();
 };
 
-const userapp=require("./models/fellow")
-app.use('/user-api',userapp)
-
+//const userapp = require("./models/fellow");
+//app.use("/user-api", userapp);
 
 // creating middleware for auth.js to convert data into json or to read im not sure
 app.use(express.json());
 // get the request sent by the user
+
 app.use(require("./router/auth"));
+
+console.log(uuid.v4());
+//Create User which is fellow
+app.post("/user-api/create", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 // listen and respond to the requesting stating the server
 const PORTS = process.env.PORT;
--- models/User.js
+++ models/User.js
@@ -38,6 +38,5 @@ const userSchema = new Schema({
   },
 });
 
-
 const user = mongoose.model("user", userSchema);
 module.exports = user;
