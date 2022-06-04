9aa18a24719ba4a1745bbdb85ff99a790cb6f236
Nimesh <nimeshjohari95@gmail.com>
2022-06-04 17:48:18 +0530

    User init

---
+++ models/userSchema.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+});
+
+const user = mongoose.model("user", userSchema);
+module.exports = user;
