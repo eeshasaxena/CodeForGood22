abf02a43e5fe9230f6f67d19a65be77634bb25df
Nimesh <nimeshjohari95@gmail.com>
2022-06-04 20:26:41 +0530

    User Update :)

--- models/DataEntry.js
+++ models/DataEntry.js
@@ -1,14 +1,49 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 // Read from csv and create a new schema
-const csv = require("csvtojson");
-const fs = require("fs");
-const path = require("path");
-const dbFile = path.join(__dirname, "../models/Dataset.csv");
-//convert csv to json
-csv()
-  .fromFile(dbFile)
-  .then((jsonObj) => {
-    console.log(jsonObj);
-    //create a new schema from fields of json
-  });
+/*
+Create a collection of the needed fields along with Id of person who added the data 
+and req.body mei usko pass kar do .
+ */
+/*
+Total Number OoSC and Out Of School Children Identified
+Total Number OoSC and Out Of Children Enrolled In Learning Center
+The Total Classes Conducted this month
+Hindi
+English,
+EVS,
+Math,
+Number of the Students attended classes regularly
+Number of exposure visits/sessions done,
+"Number of Sessions/Visits/Meetings on vision setting,
+reflection and self-awareness",
+"Number of training on classroom intervention,
+ behaviour management cycle and culture & Values",
+ Number of community visits made to identify and understand local problems
+,Number of Community event organised to mobilize stakeholders,"Number of parents attended workshops/training (PTM, SMC)",Number of parents joined/participated in awareness drives and campaigns.,Number of community members attended community events and program organized for mobilization,Number of parents visited Anubhuti learning centers,Number of parents had one on one conversations with fellows during the field visit.,Thank You for Filling the Form - Please Rate you experience or day,Location 
+*/
+const dsSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  isAuthorized: {
+    type: Boolean,
+  },
+  month: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+});
--- router/auth.js
+++ router/auth.js
@@ -9,5 +9,8 @@ const user = require("../models/User");
 router.get("/", (req, res) => {
   res.send("Sending from auth");
 });
+//login basic api
+//get all fellows for a   pa
+//get all pa from pm
 
 module.exports = router;
